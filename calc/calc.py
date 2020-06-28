b12531d205464248d7c59c336e56bd154e197d51
settledow <kccnggg@kookmin.ac.kr>
2020-06-28 18:50:21 +0900

    issue #7 first setting sum,mul -1,-1

--- calc/calc.py
+++ calc/calc.py
@@ -5,7 +5,7 @@ def application(environ, start_response):
         d = parse_qs(environ['QUERY_STRING'])
         first_num = d.get('first_num', [''])[0]
         second_num = d.get('second_num', [''])[0]
-        sum, mul = 0,0
+        sum, mul = -1,-1
         if '' not in [first_num, second_num]:
             first_num, second_num = int(first_num), int(second_num)
             sum = first_num + second_num
